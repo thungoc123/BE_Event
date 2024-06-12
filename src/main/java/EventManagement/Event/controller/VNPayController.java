2ac085ce61f84f0b91170cb16e96048c973f90ac
namzuize <namndpse171442@fpt.edu.vn>
2024-06-13 02:32:46 +0700

    test

--- src/main/java/EventManagement/Event/controller/VNPayController.java
+++ src/main/java/EventManagement/Event/controller/VNPayController.java
@@ -51,7 +51,7 @@ public class VNPayController {
             String vnp_CreateDate = formatter.format(cld.getTime());
             vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
 
-            cld.add(Calendar.MINUTE, 15);
+            cld.add(Calendar.MINUTE, 30);
             String vnp_ExpireDate = formatter.format(cld.getTime());
             vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
 
