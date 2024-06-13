db761abd5098001f3bf73ee8a313a1aaac5c6503
thungoc123 <92897465+thungoc123@users.noreply.github.com>
2024-06-14 01:38:31 +0700

    Stress

--- src/main/java/EventManagement/Event/config/WebConfig.java
+++ src/main/java/EventManagement/Event/config/WebConfig.java
@@ -1,21 +1,21 @@
-package EventManagement.Event.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
-                .allowedOrigins("http://localhost:5173","https://eventmanagementfu.azurewebsites.net/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowedHeaders("")
-                .allowCredentials(true)
-                .allowedMethods("*")
-                .maxAge(3600);
-    }
-}
+//package EventManagement.Event.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/*")
+//                .allowedOrigins("http://localhost:5173","https://eventmanagementfu.azurewebsites.net/")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+//                .allowedHeaders("")
+//                .allowCredentials(true)
+//                .allowedMethods("*")
+//                .maxAge(3600);
+//    }
+//}
